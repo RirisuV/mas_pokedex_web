38644aa62220db3ff7af48efe8ea4c5576d2c361
Marcin Krupa <s13711@pjwstk.edu.pl>
2018-05-30 16:47:13 +0200

    pokemon species model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/PokemonSpecies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class PokemonSpecies
+    {
+
+
+        public int PokemonSpeciesID { get; set; }
+
+        public int number { get; set; }
+
+        public string name { get; set; }
+
+        public string descriptionNo1 { get; set; }
+
+        public string descriptionNo2 { get; set; }
+
+        public string genre { get; set; }
+
+        public byte[] image { get; set; }
+
+    }
+}
