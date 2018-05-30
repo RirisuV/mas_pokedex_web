ff18013fee3ec990675b74b2b6533ecae44a7b6d
MarcinKrupa <s13711@gmail.com>
2018-05-30 17:55:30 +0200

    trainer model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Trainer : Person
+    {
+
+        public int money { get; set; }
+
+        public int pokemonAmount { get; set; }
+
+        public int pokemonsScanned { get; set; }
+
+    }
+
+}
