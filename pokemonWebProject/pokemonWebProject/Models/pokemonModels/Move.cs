b2b274041ee200d043a0f7f281d433a5fb0e4295
Marcin Krupa <s13711@pjwstk.edu.pl>
2018-05-30 16:50:55 +0200

    move model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Move.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Move
+    {
+
+        public int MoveID { get; set; }
+
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public int moveDamage { get; set; }
+
+    }
+}
