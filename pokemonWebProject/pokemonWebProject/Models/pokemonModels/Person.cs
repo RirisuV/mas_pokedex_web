77a71409b3225c24a6f9f9fbc0d5143f7a9c3385
Marcin Krupa <s13711@pjwstk.edu.pl>
2018-05-30 16:41:32 +0200

    person model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public String firstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public String secondName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public DateTime dateOfBirth { get; set; }
+
+    }
+}
