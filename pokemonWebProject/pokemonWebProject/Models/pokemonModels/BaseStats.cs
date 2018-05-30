40f087bd1393f67ba43ea655c5eade00dad36f6b
MarcinKrupa <s13711@gmail.com>
2018-05-30 17:37:56 +0200

    base stats model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/BaseStats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class BaseStats
+    {
+
+        public int BaseStatsID { get; set; }
+
+        public int hpStat { get; set; }
+
+        public int attackStat { get; set; }
+
+        public int defenseStat { get; set; }
+
+        public int specialAttackStat { get; set; }
+
+        public int specialDefenseStat { get; set; }
+
+        public int speed { get; set; }
+
+    }
+}
