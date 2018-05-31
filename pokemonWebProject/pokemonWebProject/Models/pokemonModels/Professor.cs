ee0d7efba0884181afc542483e92830f2fedb90b
Marcin Krupa <marcinkgm@gmail.com>
2018-05-31 21:19:27 +0200

    professor model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Professor
+    {
+
+        public string specialisation { get; set; }
+
+        public int holdedPokemonAmount { get; set; }
+
+    }
+}
