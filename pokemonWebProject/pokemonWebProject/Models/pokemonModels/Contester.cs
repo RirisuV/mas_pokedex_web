938b75a8852f0d7fa14acdef3a61544cf2873b65
Marcin Krupa <marcinkgm@gmail.com>
2018-06-07 21:53:37 +0200

    contester model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Contester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Contester : Pokemon
+    {
+
+        public string specialisation { get; set; }
+
+    }
+}
