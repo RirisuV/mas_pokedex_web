227359a2e0400dff80aded3f258aa6660ba5d86c
Marcin Krupa <marcinkgm@gmail.com>
2018-06-07 21:39:06 +0200

    gifted model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Gifted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Gifted : Pokemon
+    {
+
+        public string objections { get; set; }
+
+    }
+}
