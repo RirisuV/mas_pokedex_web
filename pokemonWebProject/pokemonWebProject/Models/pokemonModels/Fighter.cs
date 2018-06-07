8487b9adb01822f5afa4341e0e50e181780842a9
Marcin Krupa <marcinkgm@gmail.com>
2018-06-07 21:52:39 +0200

    fighter model added

---
+++ pokemonWebProject/pokemonWebProject/Models/pokemonModels/Fighter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pokemonWebProject.Models.pokemonModels
+{
+    public class Fighter : Pokemon
+    {
+        public int hpEvTrained { get; set; }
+
+        public int attackEvTrained { get; set; }
+
+        public int defenseEvTrained { get; set; }
+
+        public int specialAttackEvTrained { get; set; }
+
+        public int specialDefenseEvTrained { get; set; }
+
+        public int speedEvTrained { get; set; }
+
+    }
+}
